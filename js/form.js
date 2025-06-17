fa0e174c9cd5c06d4745c8d58c835560167c7f48
LeaMaringer <lea.maringer@gmail.com>
2025-06-18 00:03:08 +0200

    contact form update

---
+++ js/form.js
@@ -0,0 +1,15 @@
+import emailjs from '@emailjs/browser';
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm('service_yhy3ipv', 'template_sc6gj8i', this, 'SWaPHxTtpgQoTONUOAru_')
+    .then(() => {
+      alert('Your message has been sent successfully !');
+      this.reset();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert("An error occurred. Please try again.");
+    });
+});
