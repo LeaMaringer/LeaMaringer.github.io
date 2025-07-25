1df195e5b72c9941bcdb645b5583269f9d196f69
LeaMaringer <lea.maringer@gmail.com>
2025-07-25 07:19:19 +0200

    big update july 2025

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 function change_picture() {
     var picture = document.getElementById("me").src;
     if (picture.indexOf("img/photoLea.jpg") != -1) {
-        document.getElementById("me").src = "img/midjourney_picture.png";
+        document.getElementById("me").src = "img/image_deepai.jpeg";
     }
     else {
         document.getElementById("me").src = "img/photoLea.jpg";
